9cd0902020874421d3cca87f95b0a17d4062c2f9
dirtydan119 <djrodriguez119@gmail.com>
2022-01-10 16:01:58 -0500

    first commit

---
+++ backend/db.js
@@ -0,0 +1,35 @@
+const mongodb = require('mongodb')
+
+const MongoClient = mongodb.MongoClient
+const mongoDbUrl =
+  'mongodb+srv://admin:adminpass@udemy.2wusp.mongodb.net/shop?retryWrites=true&w=majority'
+
+let _db
+
+const initDb = (callback) => {
+  if (_db) {
+    // is initialized
+    console.log('Database is already initialized')
+    return callback(null, _db)
+  }
+  MongoClient.connect(mongoDbUrl)
+    .then((client) => {
+      _db = client
+      callback(null, _db)
+    })
+    .catch((err) => {
+      callback(err)
+    })
+}
+
+const getDb = () => {
+  if (!_db) {
+    throw Error('Database not initialized')
+  }
+  return _db
+}
+
+module.exports = {
+  initDb,
+  getDb,
+}
